d6e401c219f41e13035b1847a17ac2d923f6f2ae
Andresss <davaccar@ufpso.edu.co>
2025-03-11 11:47:58 -0500

    Cambios en dashboard

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -1,22 +1,15 @@
 <head>
-    <!-- FontAwesome CDN -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-
-    <!-- Otros enlaces a CSS y meta información -->
-    <!-- ... -->
 </head>
 
 <body>
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
-            <!-- Ícono de usuario con margen a la derecha -->
-            <i class="fa-solid fa-user" style="font-size: 30px; color: white; margin-right: 10px;"></i>
-            <span class="brand-text font-weight-light text-sm" style="color: white;">
-                {{ auth()->user()->email }}
+        <a href="index3.html" class="brand-link" style="text-decoration: none;">
+            <i class="fa-solid fa-user" style="font-size: 40px; color: white; margin-right: 10px;"></i>
+            <span class="brand-text font-weight-light text-sm" style="color: white; text-decoration: none;">
+                {{ auth()->user()->name }}
             </span>
         </a>
-
 
         <!-- Sidebar -->
         <div class="sidebar">
