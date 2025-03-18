c45479e3a6a1a2554d5ba20c268b80ef16fc5172
Andresss <davaccar@ufpso.edu.co>
2025-03-18 12:07:35 -0500

    Se añaden migraciones grupo 6

---
+++ database/migrations/2025_03_16_131530_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombreevento');
+            $table->text('descripcion');
+            $table->date('fecha_inicio');
+            $table->date('fecha_final');
+            $table->string('lugar');
+            $table->string('status')->default('activo');
+            $table->integer('maxparticipantes');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('eventos');
+    }
+};
